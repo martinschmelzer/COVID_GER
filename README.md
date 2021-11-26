dae0921e2c6a00c68cc1de0481f56540480a5d20
Martin Schmelzer <schmelzer.martin@gmx.de>
2021-11-26 21:55:28 +0100

    Use vaccination data from 2021-09-16. Update axis labels. Add date to name of pdf output file.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # COVID_GER
-Einfache übersichtliche Codes und Visualisierungen zum Thema COVID.
+
+## Visualisierung der 7-Tage-Hospitalisierungsinzeidenz nach Bundesland und Altersgruppe
+Es wurden bewusst die Impfquoten von 16.09.2021 gewählt. Jene, mit denen die Bundesländer
+in die 4. Welle "gestartet" sind.
--- impfquote_hospitalisierung.R
+++ impfquote_hospitalisierung.R
@@ -1,8 +1,8 @@
 require(data.table)
 require(tidyverse)
 
-# Impfquoten
-quoten <- fread("https://raw.githubusercontent.com/robert-koch-institut/COVID-19-Impfungen_in_Deutschland/master/Aktuell_Deutschland_Impfquoten_COVID-19.csv", encoding = "UTF-8")
+# Impfquoten vom 16.09.2021, vor der 4. Welle und erstes Bereitstellungsdatum
+quoten <- fread("https://github.com/robert-koch-institut/COVID-19-Impfungen_in_Deutschland/raw/master/Archiv/2021-09-17_Deutschland_Impfquoten_COVID-19.csv", encoding = "UTF-8")
 quoten <- quoten %>%
   select(Bundesland, Impfquote_gesamt_min1, Impfquote_gesamt_voll) %>%
   filter(Bundesland != "Bundesressorts")
@@ -10,7 +10,7 @@ quoten <- quoten %>%
 # Hospitalisierungsraten (ausgewählte Altersgruppen)
 df <- fread("https://raw.githubusercontent.com/robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland/master/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv", encoding = "UTF-8")
 df <- df %>%
-  filter(Bundesland != "Bundesgebiet", Altersgruppe %in% c("15-34", "35-59", "60-79", "80+"), Datum >= lubridate::ymd("20210801")) %>%
+  filter(Bundesland != "Bundesgebiet", Altersgruppe %in% c("15-34", "35-59", "60-79", "80+"), Datum >= lubridate::ymd("20210916")) %>%
   merge(x = ., y = quoten, by = "Bundesland") %>%
   mutate(Bundesland = case_when(Bundesland == "Nordrhein-Westfalen" ~ "NW",
                                 Bundesland == "Berlin" ~ "BE",
@@ -25,7 +25,7 @@ df <- df %>%
                                 Bundesland == "Sachsen-Anhalt" ~ "ST",
                                 Bundesland == "Saarland" ~ "SL",
                                 Bundesland == "Brandenburg" ~ "BB",
-                                Bundesland == "Mecklenburg-Vorpommern" ~ "NRW",
+                                Bundesland == "Mecklenburg-Vorpommern" ~ "MV",
                                 Bundesland == "Bayern" ~ "BY",
                                 Bundesland == "Baden-Württemberg" ~ "BW"))
 # Bundesland als Faktor
@@ -38,11 +38,15 @@ ggplot(df) +
   geom_line(aes(x = Datum, y = `7T_Hospitalisierung_Inzidenz`, col = Impfquote_gesamt_voll)) +
   scale_color_gradient(low = "red", high = "green", "Quote der voll-\nständigen Impfungen (%)", breaks = seq(min(df$Impfquote_gesamt_voll), max(df$Impfquote_gesamt_voll), length.out = 3)) +
   facet_grid(rows = vars(Bundesland), cols = vars(Altersgruppe)) +
-  ylab("7-Tage Hospitalisierung Inzidenz") +
+  ylab("7-Tage-Hospitalisierungsinzidenz") +
   xlab("") +
-  ggtitle("Inzidenz der 7-Tage Hospitalisierung nach Bundesland und Altersgruppe", subtitle = "Impfquoten vom 22.11.2021, Quelle: RKI") +
+  ggtitle("7-Tage-Hospitalisierungsinzidenz nach Bundesland und Altersgruppe", subtitle = "Impfquoten vom 01.10.2021, Quelle: RKI") +
   theme(text = element_text(size = 11),
         strip.text.y = element_text(angle = 0))
 dev.off()
+
+
+
+
 
 
